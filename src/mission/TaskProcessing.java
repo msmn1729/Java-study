c630a9d434f72c0b68a7ec2a7176f6725695dc3b
Geonyeol Park <msmn1729@gmail.com>
2022-04-17 03:15:04 +0900

    refactor: 메서드 추출 후 코드 정리

--- src/mission/TaskProcessing.java
+++ src/mission/TaskProcessing.java
@@ -6,10 +6,17 @@ import java.util.*;
 public class TaskProcessing {
     private static int[] tag = new int[10];
     private static int createTaskFailCnt = 0;
-    private static Map<Integer, Integer> taskExecuteFailMap = new HashMap<>(); // (key, value): (태그번호, 실패횟수)
-    private static List<Integer> taskExecuteFailList;
+    private static Map<Integer, Integer> executeTaskFailMap = new HashMap<>(); // (key, value): (태그번호, 실패횟수)
+    private static List<Integer> executeTaskFailList;
 
     public static void main(String[] args) throws IOException {
+        commandProcessing();
+        printAvailableTagList();
+        printCreateTaskFailCnt();
+        printExecuteTaskFailTagInfo();
+    }
+
+    private static void commandProcessing() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
@@ -20,23 +27,6 @@ public class TaskProcessing {
             } else if (input.contains("execute")) {
                 executeTask(input);
             }
-        }
-
-        System.out.print("사용가능한 TAG:");
-        for (int i = 1; i <= 9; i++) {
-            if (tag[i] == 0) {
-                System.out.print(" " + i);
-            }
-        }
-        System.out.println("\nTASK 생성 실패: " + createTaskFailCnt);
-
-        // 정렬을 위해 맵에 있는 키값을 리스트로 변환
-        taskExecuteFailList = new ArrayList<>(taskExecuteFailMap.keySet());
-        sortTaskExecuteFailList();
-
-        System.out.print("TASK 수행 실패한 태그:");
-        for (Integer failTagNum : taskExecuteFailList) {
-            System.out.print(" " + failTagNum + "(" + taskExecuteFailMap.get(failTagNum) + ")");
         }
     }
 
@@ -58,13 +48,13 @@ public class TaskProcessing {
 
         // 입력 태그값의 범위값(1~9)이 아닌 경우 수행 실패 처리
         if (tagNum < 1 || 9 < tagNum) {
-            setTaskExecuteFailMap(tagNum);
+            setExecuteTaskFailMap(tagNum);
             return; // 메서드 종료
         }
 
         // 요청들어온 태그가 미등록(0)인 경우 수행 실패 처리
         if (tag[tagNum] == 0) {
-            setTaskExecuteFailMap(tagNum);
+            setExecuteTaskFailMap(tagNum);
         } else {
             // 태스크 수행 성공 -> 태그 번호 미등록(0)으로 업데이트
             tag[tagNum] = 0;
@@ -72,21 +62,45 @@ public class TaskProcessing {
     }
 
     // 수행 실패한 태그번호를 실패 횟수 카운팅해서 맵에 저장하는 메서드
-    private static void setTaskExecuteFailMap(int tagNum) {
-        if (taskExecuteFailMap.containsKey(tagNum)) {
-            taskExecuteFailMap.put(tagNum, taskExecuteFailMap.get(tagNum) + 1);
+    private static void setExecuteTaskFailMap(int tagNum) {
+        if (executeTaskFailMap.containsKey(tagNum)) {
+            executeTaskFailMap.put(tagNum, executeTaskFailMap.get(tagNum) + 1);
         } else {
-            taskExecuteFailMap.put(tagNum, 1);
+            executeTaskFailMap.put(tagNum, 1);
+        }
+    }
+
+    private static void printAvailableTagList() {
+        System.out.print("사용가능한 TAG:");
+        for (int i = 1; i <= 9; i++) {
+            if (tag[i] == 0) {
+                System.out.print(" " + i);
+            }
+        }
+    }
+
+    private static void printCreateTaskFailCnt() {
+        System.out.println("\nTASK 생성 실패: " + createTaskFailCnt);
+    }
+    
+    private static void printExecuteTaskFailTagInfo() {
+        // 정렬을 위해 맵에 있는 키값을 리스트로 변환
+        executeTaskFailList = new ArrayList<>(executeTaskFailMap.keySet());
+        sortExecuteTaskFailList();
+
+        System.out.print("TASK 수행 실패한 태그:");
+        for (Integer failTagNum : executeTaskFailList) {
+            System.out.print(" " + failTagNum + "(" + executeTaskFailMap.get(failTagNum) + ")");
         }
     }
 
     // 밸류(태그 실패 횟수) 내림차순 정렬, 밸류가 동일하다면 키(태그) 오름차순 정렬하는 메서드
-    private static void sortTaskExecuteFailList() {
-        taskExecuteFailList.sort((tagNum1, tagNum2) -> {
-            if (Objects.equals(taskExecuteFailMap.get(tagNum1), taskExecuteFailMap.get(tagNum2))) {
+    private static void sortExecuteTaskFailList() {
+        executeTaskFailList.sort((tagNum1, tagNum2) -> {
+            if (Objects.equals(executeTaskFailMap.get(tagNum1), executeTaskFailMap.get(tagNum2))) {
                 return tagNum1 - tagNum2;
             } else {
-                return taskExecuteFailMap.get(tagNum2) - taskExecuteFailMap.get(tagNum1);
+                return executeTaskFailMap.get(tagNum2) - executeTaskFailMap.get(tagNum1);
             }
         });
     }
