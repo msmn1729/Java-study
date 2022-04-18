e9cbb8a6f647820472a7947b7b8d521741fc0970
Geonyeol Park <msmn1729@gmail.com>
2022-04-18 23:39:10 +0900

    feat: Tag 클래스를 분리하는 이유 작성

--- src/mission/taskProcessing_OOP/Tag.java
+++ src/mission/taskProcessing_OOP/Tag.java
@@ -3,6 +3,7 @@ package mission.taskProcessing_OOP;
 import java.util.PriorityQueue;
 
 public class Tag {
+    // OOP 개념: 사용자의 입력이 바뀌어서 태그의 자료형이 변해도 Tag 클래스만 수정하면 되도록 구현해야함
     PriorityQueue<Integer> tagQueue = new PriorityQueue<>();
 
     public Tag() {
