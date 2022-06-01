7acbc7f059488647e8dfed5bdff41eb383925798
Geonyeol Park <msmn1729@gmail.com>
2022-06-01 23:55:16 +0900

    feat: Person 클래스 생성

---
+++ src/mission/accountTransfer/Person.java
@@ -0,0 +1,13 @@
+package mission.accountTransfer;
+
+public class Person {
+    private String name;
+    private int age;
+    private String gender;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
