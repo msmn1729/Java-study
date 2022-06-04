4f88eab7374eb75d7863fe69f7d6d67cd38c6e1a
Geonyeol Park <msmn1729@gmail.com>
2022-06-04 20:42:37 +0900

    feat: amount, balance 값의 유효성 체크 기능 수정

--- src/mission/accountTransfer/AccountActivity.java
+++ src/mission/accountTransfer/AccountActivity.java
@@ -8,30 +8,46 @@ public class AccountActivity {
 
     // 예금
     public void deposit(Account account, int amount) {
+        if (!isAmountCheck(amount)) {
+            return;
+        }
         account.setBalance(account.getBalance() + amount);
         records.add("[예금성공] ID: " + account.getId() + ", 예금금액: " + amount + ", 잔액: " + account.getBalance());
     }
 
     // 출금
     public void withdraw(Account account, int amount) {
-        int balance = account.getBalance() - amount;
-        if (balance < 0) {
+        if (!isAmountCheck(amount)) {
+            return;
+        }
+        if (!isBalanceCheck(account.getBalance(), amount)) {
             records.add("[출금실패] ID: " + account.getId() + "의 잔액이 부족합니다.");
             return;
         }
-        account.setBalance(balance);
+        account.setBalance(account.getBalance() - amount);
         records.add("[출금성공] ID: " + account.getId() + ", 예금금액: " + amount + ", 잔액: " + account.getBalance());
     }
 
     // 이체
     public void transfer(Account accountFrom, Account accountTo, int amount) {
-        int balance = accountFrom.getBalance() - amount;
-        if (balance < 0) {
-            records.add("[이체실패] ID: " + accountFrom.getId() + "의 잔액이 부족합니다.");
+        if (!isAmountCheck(amount)) {
             return;
         }
+        withdraw(accountFrom, amount);
         deposit(accountTo, amount);
         records.add("[이체성공] 보낸ID: " + accountFrom.getId() + ", 받은ID: " + accountTo.getId() + ", 보낸금액: " + amount);
+    }
+
+    public boolean isAmountCheck(int amount) {
+        if (amount < 0) {
+            records.add("금액에 음수를 입력할 수 없습니다.");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isBalanceCheck(int balance, int amount) {
+        return balance >= amount;
     }
 
     // 거래내역 조회
